946b1fe6f4f8cef1d0df88609f1bc1b47a4786fa
bimadmrt <56182626+bimadmrt@users.noreply.github.com>
2020-03-03 19:26:06 +0700

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="container">
+    <div class="container">
+      <div class="container">
+      </div>
+    </div>
+  </div>
+</template>
+<style>
+html,body {
+  height : 100%;
+}
+.container{
+  height: 100%;
+  border: 10px solid blue;
+  padding: 100px!important;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
